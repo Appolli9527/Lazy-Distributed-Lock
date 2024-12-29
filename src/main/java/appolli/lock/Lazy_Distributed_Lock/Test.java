3af7fa9f38849770ba76521c24bdcb5f6e1d433c
zhangshu <70257536+Appolli9527@users.noreply.github.com>
2024-12-29 16:36:35 +0800

    first commit

---
+++ src/main/java/appolli/lock/Lazy_Distributed_Lock/Test.java
@@ -0,0 +1,10 @@
+package appolli.lock.Lazy_Distributed_Lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.info("start");
+    }
+}
